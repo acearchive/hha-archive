d174aebee9dc8d205f93e6a4e6180b4e047c04ef
Frawley <frawleyskid@gmail.com>
2022-05-14 11:48:42 -0400

    Set locale explicitly

--- build.sh
+++ build.sh
@@ -5,7 +5,13 @@ set -e
 base_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 cd "$base_dir/yg-render/parser"
-go run . -o "$base_dir/output" -t "Haven for the Human Amoeba" -b "https://hha.acearchive.lgbt/" --link "archive,Ace Archive,https://acearchive.lgbt/artifact/haven-for-the-human-amoeba/" "$base_dir/archive"
+
+go run . "$base_dir/archive" \
+    --output "$base_dir/output" \
+    --title "Haven for the Human Amoeba" \
+    --base "https://hha.acearchive.lgbt/" \
+    --link "archive,Ace Archive,https://acearchive.lgbt/artifact/haven-for-the-human-amoeba/" \
+    --locale "en_US"
 
 export OUTPUT_DIR="$base_dir/output"
 export PUBLIC_DIR="$base_dir/public"
