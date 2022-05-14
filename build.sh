9f8947c092b7a3d3fe766ea598b3b90298b683e8
Frawley <frawleyskid@gmail.com>
2022-05-14 02:25:43 -0400

    Add external link to Ace Archive artifact page

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 base_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 cd "$base_dir/yg-render/parser"
-go run . -o "$base_dir/output" -t "Haven for the Human Amoeba" -b "https://hha.acearchive.lgbt/" "$base_dir/archive"
+go run . -o "$base_dir/output" -t "Haven for the Human Amoeba" -b "https://hha.acearchive.lgbt/" --link "archive,Ace Archive,https://acearchive.lgbt/artifact/haven-for-the-human-amoeba/" "$base_dir/archive"
 
 export OUTPUT_DIR="$base_dir/output"
 export PUBLIC_DIR="$base_dir/public"
